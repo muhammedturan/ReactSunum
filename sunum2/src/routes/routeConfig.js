a6d885ca4d3eece3a38eab3766bb753199f7e225
muhammedturan <muhammedturan@gmail.com>
2022-10-21 14:13:56 +0300

    zxsdcx

--- sunum2/src/routes/routeConfig.js
+++ sunum2/src/routes/routeConfig.js
@@ -2,9 +2,8 @@ import AddCustomer from "../view/customer/AddCustomer";
 import CustomerDetail from "../view/customer/CustomerDetail";
 import CustomerList from "../view/customer/CustomerList";
 import UpdateCustomer from "../view/customer/UpdateCustomer";
-import Dashboard from "../view/layout/Dashboard";
-import Index from "../view/layout/Index";
-import Login from "../view/layout/Login";
+import Index from "../view/customer/Index";
+
 
 export const routeConfig =
     [
